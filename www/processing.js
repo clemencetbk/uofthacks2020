e29090a33e5eedb3b1736d011a59bdbdec3371e3
clemencetbk <kohclemence@gmail.com>
2020-01-19 07:22:43 -0500

    Add initial getPhysicalCloseness() with to-do

--- www/processing.js
+++ www/processing.js
@@ -25,3 +25,32 @@ function getDigitalCloseness(msg_json) {
     }
     return points;
 }
+
+function getPhysicalCloseness(loc_json1, loc_json2) {
+    let points = [];
+    let locations1 = loc_json1["locations"];
+    let locations2 = loc_json2["locations"];
+    // Sample location for all days available
+    // Get distance between points
+    // Return array of (days, distance)
+    return points;
+}
+
+
+// https://stackoverflow.com/questions/27928/calculate-distance-between-two-latitude-longitude-points-haversine-formula
+function getDistance(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2); 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+}
+
+function deg2rad(deg) {
+    return deg * (Math.PI/180)
+}
